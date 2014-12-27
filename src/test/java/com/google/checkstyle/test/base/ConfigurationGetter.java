889ad48d0d08e5941cad20924656626ca0068931
Roman Ivanov <ivanov-jr@mail.ru>
2014-12-27 15:08:24 -0800

    changes for google config location

--- src/test/java/com/google/checkstyle/test/base/ConfigurationGetter.java
+++ src/test/java/com/google/checkstyle/test/base/ConfigurationGetter.java
@@ -10,6 +10,7 @@ import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 
 public class ConfigurationGetter {
 
+	private static String config = "https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml";
 	private static boolean updateStatus;
 
 	public static final ConfigurationGetter INSTANCE = new ConfigurationGetter();
@@ -34,7 +35,7 @@ public class ConfigurationGetter {
 
 	private static void getConfiguration() throws CheckstyleException, IOException {
 		final String xmlName = "google_checks.xml";
-		final URL configUrl = new URL("https://raw.githubusercontent.com/checkstyle/checkstyle/master/google_checks.xml");
+		final URL configUrl = new URL(config);
 		FileUtils.copyURLToFile(configUrl, new File(xmlName));
 		updateStatus = true;
 	}
